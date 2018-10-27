13308ddbde8d52d726d00d93936ee2d4d9ab9e10
Ethan <ethan@ek-macbook-air.local>
2018-10-26 21:13:22 -0400

    Initial Commit

---
+++ Inverted Index/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Inverted Index
+//
+//  Created by Ethan on 10/26/18.
+//  Copyright © 2018 Ethan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
